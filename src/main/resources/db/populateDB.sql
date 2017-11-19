dedc01871cc32d90f4920baaaf574eeae67594d7
APashkov <a.pashkov@bk.ru>
2017-11-19 22:27:20 +0300

    Add sql script and JdbcMealRepositoryImpl

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,14 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+
+INSERT INTO topjava.public.meals (user_id, date_time, description, calories) VALUES
+  (100000, '2017-11-19 07:00', 'Завтрак', DEFAULT),
+  (100000, '2017-11-19 12:00', 'Обед', 1000),
+  (100000, '2017-11-19 19:00', 'Ужин', 700),
+  (100000, DEFAULT , 'Перекус', 100),
+  (100001, '2017-11-19 08:00', 'Завтрак', DEFAULT),
+  (100001, '2017-11-19 12:00', 'Обед', 1100),
+  (100001, '2017-11-19 19:00', 'Ужин', 500),
+  (100001, DEFAULT , 'Перекус', 100);
