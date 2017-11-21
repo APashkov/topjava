a27707069733235c5c75d3c6435b52d2939d15eb
APashkov <a.pashkov@bk.ru>
2017-11-21 23:58:02 +0300

    Add MealServiceTest

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -7,16 +7,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
-import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.util.DateTimeUtil;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 @ContextConfiguration({
@@ -32,41 +28,46 @@ public class MealServiceTest {
 
     @Test
     public void get() throws Exception {
-        service.get(START_SEQ+2, UserTestData.USER_ID);
+        service.get(START_SEQ+2, START_SEQ);
     }
 
     @Test
     public void delete() throws Exception {
-        service.delete(START_SEQ+2, UserTestData.USER_ID);
+        service.delete(START_SEQ+2, START_SEQ);
     }
 
     @Test
     public void getBetweenDates() throws Exception {
         service.getBetweenDates(LocalDate.of(2017, 11, 19), LocalDate.of(2017,
-                11,21), UserTestData.USER_ID);
+                11,21), START_SEQ);
     }
 
     @Test
     public void getBetweenDateTimes() throws Exception {
         service.getBetweenDateTimes(LocalDateTime.of(2017, 11, 19,8, 00),
-                LocalDateTime.of(2017, 11, 20, 18, 00), UserTestData.USER_ID);
+                LocalDateTime.of(2017, 11, 20, 18, 00), START_SEQ);
     }
 
     @Test
     public void getAll() throws Exception {
-        service.getAll(UserTestData.USER_ID);
+        service.getAll(START_SEQ);
     }
 
     @Test
     public void update() throws Exception {
         Meal updated = new Meal(null, LocalDateTime.now(),"NewMeal", 1000);
-        service.update(updated, UserTestData.USER_ID);
+        service.update(updated, START_SEQ);
     }
-
+/*
+    public static void assertMatch(User actual, User expected) {
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "registered", "roles");
+    }
+*/
     @Test
     public void create() throws Exception {
         Meal newMeal = new Meal(null, LocalDateTime.now(),"NewMeal", 1000);
-        Meal created = service.create(newMeal, UserTestData.USER_ID);
+        Meal created = service.create(newMeal, START_SEQ);
         newMeal.setId(created.getId());
+        //assertThat(newMeal).isEqualToIgnoringGivenFields(newMeal, "date_time");
     }
 }
